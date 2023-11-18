66b150de9d1a607f194fc0bf210d04d26d1813f3
Marcello <hey@marcello.codes>
2023-11-18 18:06:03 -0500

    🔗 Fix a broken link

--- src/app/views/homepage/homepage.component.ts
+++ src/app/views/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HomepageLayoutComponent } from '../../layouts/homepage-layout/homepage-layout.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CommonModule, HomepageLayoutComponent],
+  imports: [CommonModule, RouterModule, HomepageLayoutComponent],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
