77bb8aaeea658acbfe75c9099f9d2c3375960111
Marcello <hey@marcello.codes>
2023-12-08 13:41:29 -0500

    Fix budgets

--- src/app/services/browser-storage.service.ts
+++ src/app/services/browser-storage.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class BrowserStorageService {
-  constructor() {}
   get(key: string) {
     return localStorage.getItem(key);
   }
