aa91483e73eb70b5897850e866d796a06671d85c
Marcello <hey@marcello.codes>
2023-12-15 15:04:11 -0500

    Some more updates

--- src/app/views/accounts/show/show-account.component.ts
+++ src/app/views/accounts/show/show-account.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { BankAccount } from '../../../models/bank-account.model';
+import { BannerComponent } from '../../../components/banner/banner.component';
 
 @Component({
   selector: 'app-show-account',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BannerComponent],
   templateUrl: './show-account.component.html',
   styleUrl: './show-account.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
